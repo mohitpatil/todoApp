12137dbeeb10a7ddd59bfe6016e7e513c4d2f7f9
Mohit <mohit.patil@terafinainc.com>
2019-02-26 14:04:07 -0800

    Updates

---
+++ precache-manifest.e14beb9972cf783442e121ff384be203.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e0dbb3538220f12a21d",
+    "url": "/todoApp/static/js/runtime~main.6e0dbb35.js"
+  },
+  {
+    "revision": "3f7880a8e5be194cc96b",
+    "url": "/todoApp/static/js/main.3f7880a8.chunk.js"
+  },
+  {
+    "revision": "00fe26e1e22ae327abf6",
+    "url": "/todoApp/static/js/2.00fe26e1.chunk.js"
+  },
+  {
+    "revision": "3f7880a8e5be194cc96b",
+    "url": "/todoApp/static/css/main.2866b1bc.chunk.css"
+  },
+  {
+    "revision": "00fe26e1e22ae327abf6",
+    "url": "/todoApp/static/css/2.988ee7af.chunk.css"
+  },
+  {
+    "revision": "0e80c017afc0f1b7dcac700341ec6d1d",
+    "url": "/todoApp/index.html"
+  }
+];
